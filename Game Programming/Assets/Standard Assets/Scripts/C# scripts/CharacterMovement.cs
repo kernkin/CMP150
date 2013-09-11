6f0b71fffec63cebac3a241cc570cbe818983839
Kernkin <saul.jordan@student.ccm.edu>
2013-09-10 21:01:11 -0400

    9/10/13 work and assignment is progress

--- Game Programming/Assets/Standard Assets/Scripts/C# scripts/CharacterMovement.cs
+++ Game Programming/Assets/Standard Assets/Scripts/C# scripts/CharacterMovement.cs
@@ -1,22 +1,86 @@
+
+
 using UnityEngine;
 using System.Collections;
+using System;
 
-public class CharacterMovement : MonoBehaviour
+
+[Serializable]
+public class CharacterControls
 {
-
-    public float moveSpeed = 5f;
-    // Use this for initialization
-    void Start()
+    public Control MoveForward; 
+    public Control MoveBack;
+    public Control MoveLeft;
+    public Control MoveRight;
+}
+public class MyDerivedMono : MonoBehaviour
+{
+    public float XPos
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
+        get { return transform.position.x; }
+        set
         {
-            transform.Translate(transform.forward * (moveSpeed * Time.deltaTime));
+            Vector3 myPos = transform.position;
+            myPos.x = value;
+            transform.position = myPos;
         }
     }
+    public float YPos
+    {
+        get { return transform.position.y; }
+        set
+        {
+            Vector3 myPos = transform.position;
+            myPos.y = value;
+            transform.position = myPos;
+        }
+    }
+
+
 }
+public class CharacterMovement : MyDerivedMono
+{
+
+
+    public float MoveSpeed = 5f;
+    public CharacterControls controls = new CharacterControls();
+
+
+    private float trueSpeed
+    {
+        get { return MoveSpeed * Time.deltaTime; }
+    }
+	// Use this for initialization
+	void Start () 
+    {
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+    {
+        //XPos = 100f;
+
+
+        if (controls.MoveForward.IsActive)
+        {
+            transform.Translate(transform.forward * trueSpeed);
+        }
+        if (controls.MoveBack.IsActive)
+        {
+            transform.Translate(transform.forward *-1 * trueSpeed);
+        }
+        if (controls.MoveLeft.IsActive)
+        {
+            transform.Translate(transform.right * -1 * trueSpeed);
+        }
+        if (controls.MoveRight.IsActive)
+        {
+            transform.Translate(transform.right * trueSpeed);
+        }
+	}
+}
+
+ 
